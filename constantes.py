b397d2893f8489850e9fb573c17f73bd209fb166
fabianarsff <fabianarsantos@gmail.com>
2023-06-01 13:36:45 -0300

    jogo Pacman

---
+++ constantes.py
@@ -0,0 +1,21 @@
+LARGURA = 480
+ALTURA = 600
+
+TITULO_JOGO = 'PACMAN'
+
+FPS = 30
+
+PRETO = (0, 0, 0)
+
+SPRITESHEET = 'spritesheet.png'
+
+PACMAN_START_LOGO = 'pacman-logo-1.png'
+
+FONTE = 'arial'
+
+AMARELO = (244, 233, 51)
+
+BRANCO = (255, 255, 255)
+
+MUSICA_START = 'intermission.wav'   
+TECLA_START = 'munch_1.wav'
---
+++ principal.py
@@ -0,0 +1,119 @@
+import os
+
+import pygame
+
+import constantes
+import sprites
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.tela = pygame.display.set_mode(
+            (constantes.LARGURA, constantes.ALTURA)
+        )
+        pygame.display.set_caption(constantes.TITULO_JOGO)
+        self.relogio = pygame.time.Clock()
+        self.esta_rodando = True
+        self.fonte = pygame.font.match_font(constantes.FONTE)
+        self.carregar_arquivos()
+
+    def novo_jogo(self):
+        self.todas_as_sprites = pygame.sprite.Group()
+        self.rodar()
+
+    def rodar(self):
+        self.jogando = True
+        while self.jogando:
+            self.relogio.tick(constantes.FPS)
+            self.eventos()
+            self.atualizar_sprites()
+            self.desenhar_sprites()
+
+    def eventos(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.jogando:
+                    self.jogando = False
+                self.esta_rodando = False
+
+    def atualizar_sprites(self):
+        self.todas_as_sprites.update()
+
+    def desenhar_sprites(self):
+        self.tela.fill(constantes.PRETO)
+        self.todas_as_sprites.draw(self.tela)
+        pygame.display.flip()
+
+    def carregar_arquivos(self):
+        diretorio_imagens = os.path.join(os.getcwd(), "imagens")
+        self.diretorio_audios = os.path.join(os.getcwd(), "audios")
+        self.spritesheet = os.path.join(
+            diretorio_imagens, constantes.SPRITESHEET)
+        self.pacman_start_logo = os.path.join(
+            diretorio_imagens, constantes.PACMAN_START_LOGO)
+        self.pacman_start_logo = pygame.image.load(
+            self.pacman_start_logo).convert()
+
+    def mostrar_texto(self, texto, tamanho, cor, x, y):
+        fonte = pygame.font.Font(self.fonte, tamanho)
+        texto = fonte.render(texto, True, cor)
+        texto_rect = texto.get_rect()
+        texto_rect.midtop = (x, y)
+        self.tela.blit(texto, texto_rect)
+
+    def mostrar_start_logo(self, x, y):
+        start_logo_rect = self.pacman_start_logo.get_rect()
+        start_logo_rect.midtop = (x, y)
+        self.tela.blit(self.pacman_start_logo, start_logo_rect)
+
+    def mostrar_tela_start(self):
+        pygame.mixer.music.load(
+            os.path.join(self.diretorio_audios, constantes.MUSICA_START))
+        pygame.mixer.music.play()
+
+        self.mostrar_start_logo(constantes.LARGURA/2, 20)
+        self.mostrar_texto(
+            "Pressione qualquer tecla pra iniciar.",
+            28,
+            constantes.AMARELO,
+            constantes.LARGURA/2,
+            320,
+        )
+        self.mostrar_texto(
+            "Desenvolvido por FabianaRSFF.",
+            24,
+            constantes.AMARELO,
+            constantes.LARGURA/2,
+            560,
+        )
+        pygame.display.flip()
+        self.esperar_por_jogador()
+
+    def esperar_por_jogador(self):
+        esperando = True
+        while esperando:
+            self.relogio.tick(constantes.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    esperando = False
+                    self.esta_rodando = False
+                if event.type == pygame.KEYUP:
+                    esperando = False
+                    pygame.mixer.music.stop()
+                    pygame.mixer.Sound(
+                        os.path.join(self.diretorio_audios,
+                                     constantes.TECLA_START)
+                    ).play()
+
+    def mostrar_tela_game_over(self):
+        pass
+
+
+g = Game()
+g.mostrar_tela_start()
+
+while g.esta_rodando:
+    g.novo_jogo()
+    g.mostrar_tela_game_over()
